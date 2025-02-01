8be044dd6a28b69916e2b6db75760faeb7ed8db4
Ariyan Sidiq <128843678+sidiqariyan@users.noreply.github.com>
2025-02-01 16:34:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", gmailSender);
 
-const client = new Client({ authStrategy: new LocalAuth() });
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: { args: ["--no-sandbox", "--disable-setuid-sandbox"] },
+});
 
 client.on("qr", (qr) => {
   qrcode.toDataURL(qr, (err, url) => {
