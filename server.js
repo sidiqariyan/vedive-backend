479125ef9ba2a1866e64ac01a3e6a72d762a7bd0
Ariyan Sidiq <128843678+sidiqariyan@users.noreply.github.com>
2025-03-23 03:33:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(limiter);
 
 // Updated CORS Configuration: Allow requests from both localhost and Netlify domain
 app.use(cors({
-  origin: ["http://localhost:5173", "https://thriving-twilight-ee164e.netlify.app/"],
+  origin: ["http://localhost:5173", "https://thriving-twilight-ee164e.netlify.app"],
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true, // Allows cookies/auth headers
