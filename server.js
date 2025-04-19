ebddd757591c33df8b220494562c142c8a39e565
Ariyan Sidiq <128843678+sidiqariyan@users.noreply.github.com>
2025-04-19 15:50:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,12 +32,14 @@ connectDB();
 app.use(helmet());
 
 // Use CORS middleware to allow requests from any origin
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://vedive.com', 'http://vedive.com'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static(publicDir));
