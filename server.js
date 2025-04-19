6be768e437b87dc72908ed0f6fc5fbcdcbbad8cb
Ariyan Sidiq <128843678+sidiqariyan@users.noreply.github.com>
2025-04-19 12:24:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(helmet());
 // Use CORS middleware to allow requests from any origin
 app.use(
   cors({
-    origin: true,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
