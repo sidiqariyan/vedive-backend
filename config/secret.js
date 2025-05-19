0fd1e31a8f46caba6c400eb8225d98c8bd6bc039
Ariyan Sidiq <128843678+sidiqariyan@users.noreply.github.com>
2025-05-19 15:54:48 +0530

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,10 @@
-// config/secret.js
+require('dotenv').config();
+
 module.exports = {
-    app_id: process.env.CASHFREE_APP_ID || "TEST104968834bd3c3d83c6773fc85bc38869401",
-    secret_key: process.env.CASHFREE_SECRET_KEY || "cfsk_ma_test_d1a74a054c08cc810f652d8276503391_2dfa449b"
-  };
-  
+  app_id: process.env.CASHFREE_APP_ID,
+  secret_key: process.env.CASHFREE_SECRET_KEY,
+  // Default to production for production-level integration
+  environment: process.env.CASHFREE_ENV || 'PROD',
+  frontend_url: process.env.FRONTEND_URL,
+  notify_url: process.env.CASHFREE_NOTIFY_URL,
+};
