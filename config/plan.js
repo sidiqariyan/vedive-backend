96fcd9878f0516af3cb12db3de64edc6d8c56d78
Ariyan Sidiq <128843678+sidiqariyan@users.noreply.github.com>
2025-05-19 15:55:48 +0530

    Create plan.js

---
+++ config/plan.js
@@ -0,0 +1,8 @@
+module.exports = {
+  PLANS: {
+    free:    { price: 0,     duration: 0 },
+    daily:   { price: 99,    duration: 1  * 24 * 60 * 60 * 1000 },
+    weekly:  { price: 499,   duration: 7  * 24 * 60 * 60 * 1000 },
+    monthly: { price: 1999,  duration: 30 * 24 * 60 * 60 * 1000 },
+  },
+};
