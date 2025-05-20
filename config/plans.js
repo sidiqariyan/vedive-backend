aaca4805fa299bdb6d2e441221a5bd8b8da84bfe
Ariyan Sidiq <128843678+sidiqariyan@users.noreply.github.com>
2025-05-20 11:27:15 +0530

    Update plans.js

--- config/plans.js
+++ config/plans.js
@@ -1,8 +1,8 @@
 module.exports = {
   PLANS: {
     free:    { price: 0,     duration: 0 },
-    daily:   { price: 99,    duration: 1  * 24 * 60 * 60 * 1000 },
-    weekly:  { price: 499,   duration: 7  * 24 * 60 * 60 * 1000 },
-    monthly: { price: 1999,  duration: 30 * 24 * 60 * 60 * 1000 },
+    starter:   { price: 99,    duration: 1  * 24 * 60 * 60 * 1000 },
+    business:  { price: 499,   duration: 7  * 24 * 60 * 60 * 1000 },
+    enterprise: { price: 1999,  duration: 30 * 24 * 60 * 60 * 1000 },
   },
 };
