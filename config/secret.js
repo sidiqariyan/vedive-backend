bbf5ec07e4217a7f513c98aae16c3e8e4cdbefea
Ariyan Sidiq <128843678+sidiqariyan@users.noreply.github.com>
2025-05-20 14:43:27 +0530

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-  appId: process.env.CASHFREE_APP_ID,
-  secretKey: process.env.CASHFREE_SECRET_KEY,
+  appId: "92091559e09e1ef5eb102b66b4519029"
+  secretKey: "cfsk_ma_prod_952ee152bb1a344252f96a977558f926_f8ec5951",
   frontendUrl: process.env.FRONTEND_URL,
   notifyUrl: process.env.CASHFREE_NOTIFY_URL,
   apiBase: 'https://api.cashfree.com/pg',
