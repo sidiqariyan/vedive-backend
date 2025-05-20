71d823f8ad94fe5b63eafb782e3cb45c85a48c67
Ariyan Sidiq <128843678+sidiqariyan@users.noreply.github.com>
2025-05-20 14:43:53 +0530

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  appId: "92091559e09e1ef5eb102b66b4519029"
+  appId: "92091559e09e1ef5eb102b66b4519029",
   secretKey: "cfsk_ma_prod_952ee152bb1a344252f96a977558f926_f8ec5951",
   frontendUrl: process.env.FRONTEND_URL,
   notifyUrl: process.env.CASHFREE_NOTIFY_URL,
